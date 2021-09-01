a0c92f472c5d26edb57d75d5cb0d0b7491a82a97
Luiz Fernando C. Modolo <modolo.luizfernando@gmail.com>
2021-09-01 16:49:14 -0300

    requisito número 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,12 +30,18 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.deepStrictEqual(Array.isArray(productDetails('agua', 'sol')), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.deepStrictEqual(Object.keys(productDetails('agua', 'sol')).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof Object.entries(productDetails('agua', 'sol')), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(Object.values(productDetails('agua', 'sol'))[0], Object.values(productDetails('agua', 'sol'))[1]);
     // Teste que os dois productIds terminam com 123.
+    assert.deepStrictEqual(productDetails('agua', 'sol')[0].details.productId.endsWith('123'), true)
+    assert.deepStrictEqual(productDetails('agua', 'sol')[1].details.productId.endsWith('123'), true)
   });
 });
